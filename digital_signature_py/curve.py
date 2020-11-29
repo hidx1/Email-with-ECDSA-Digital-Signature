9cf622b4a3c6ab05851e8f6fab1fdba603e7bd0a
Ferdy <13517116@std.stei.itb.ac.id>
2020-11-29 22:14:00 +0700

    Add Curve

---
+++ digital_signature_py/curve.py
@@ -0,0 +1,29 @@
+from .point import Point
+
+
+class CurveFp:
+
+    def __init__(self):
+        self.A = 0x0000000000000000000000000000000000000000000000000000000000000000
+        self.B = 0x0000000000000000000000000000000000000000000000000000000000000007
+        self.P = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
+        self.N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+        self.G = Point(
+            0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+            0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+        )
+        self.name = "secp256k1"
+        self.nistName = nistName
+        self.oid = (1, 3, 132, 0, 10)
+
+    def contains(self, p):
+        """
+        Verify if the point `p` is on the curve
+
+        :param p: Point p = Point(x, y)
+        :return: boolean
+        """
+        return (p.y**2 - (p.x**3 + self.A * p.x + self.B)) % self.P == 0
+
+    def length(self):
+        return (1 + len("%x" % self.N)) // 2
